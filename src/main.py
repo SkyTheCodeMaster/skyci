516dfe15d422dbe823d8a47846dd7863e862bf20
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-02-03 23:20:02 -0500

    Add multi file app thing for aiohttp

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+import importlib.util
+from typing import Optional
+
 import aiohttp
 from aiohttp import web
 
+class MultiFileApplication(web.Application):
+  def load_extension(self,name:str,*,package:Optional[str]=None):
+    try:
+      n = importlib.util.resolve_name(name,package)
+      spec = importlib.util.find_spec(n)
+      lib = importlib.util.module_from_spec(spec)
+      spec.loader.exec_module(lib)
+      lib.setup(self)
+    except Exception as e:
+      raise
+
+app = MultiFileApplication()
+
+app.load_extension("cogs.frontend")
+
+web.run_app(app,port=36750)
