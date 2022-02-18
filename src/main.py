02786e28e7314cc9208da9579c346055c10cf533
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-02-17 19:31:02 -0500

    move jinja setup to __init__

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ import aiohttp_jinja2
 import jinja2
 
 class MultiFileApplication(web.Application):
+  def __init__(self,*args,**kwargs):
+    super().__init__(*args,**kwargs)
+    aiohttp_jinja2.setup(self,loader=jinja2.FileSystemLoader("src/templates"))
+
   def _get_module(self,name:str,*,package=None):
     n = importlib.util.resolve_name(name,package)
     spec = importlib.util.find_spec(n)
@@ -22,7 +26,6 @@ class MultiFileApplication(web.Application):
       raise
 
 app = MultiFileApplication()
-aiohttp_jinja2.setup(app,loader=jinja2.FileSystemLoader("src/templates"))
 
 app.load_extension("cogs.frontend")
 app.load_extension("cogs.error")
